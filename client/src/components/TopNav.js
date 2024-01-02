bf9469aa73009f0ca80a3376dd7ef9b2b4dc8174
evgeni.stanchev <evgeni.stanchev@amusnet.com>
2024-01-02 15:01:40 +0200

    refactor: remove node_modules

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,27 +1,18 @@
 import React from "react";
-// import {
-//   AiOutlineMenu,
-//   // AiOutlineSearch,
-//   // AiOutlineClose,
-//   // AiFillTag,
-// } from "react-icons/ai";
+import {
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineClose,
+  AiFillTag,
+} from "react-icons/ai";
+import { BsFillCartFill, BsPerson } from "react-icons/bs";
 
 const TopNav = () => {
   return (
     <div className="max-w-[1520] mx-auto flex justify-between items-center p-4">
       <div className="flex items-center">
         <div className="cursor-pointer">
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            stroke-width="0"
-            viewBox="0 0 1024 1024"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path>
-          </svg>
+          <AiOutlineMenu size={25} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
           Yum
@@ -34,7 +25,18 @@ const TopNav = () => {
           <p className="p-2 text-bold">Delivery</p>
         </div>
       </div>
-      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]"></div>
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+        <AiOutlineSearch size={25} />
+        <input
+          className="bg-transparent p-2 w-full focus:outline-none"
+          type="text"
+          placeholder="search meals"
+        ></input>
+      </div>
+      <button className="bg-orange-700 text-white hidden md:flex items-center py-2 rounded-full">
+        <BsFillCartFill size={20} />
+        Cart
+      </button>
     </div>
   );
 };
