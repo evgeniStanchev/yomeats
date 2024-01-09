afb0781c1ff28913160c58a8d44a805d65ae42ad
evgeni.stanchev <evgeni.stanchev@amusnet.com>
2024-01-09 11:31:00 +0200

    feat: add react-splide, top picks slider and top picks objects with their price

---
+++ client/src/data/data.js
@@ -0,0 +1,219 @@
+export const topPicks = [
+  {
+    id: 1,
+    title: "Shawarma",
+    price: "$850",
+    img: "assets/chiken_shawarma.jpg",
+  },
+  {
+    id: 2,
+    title: "Fruite Juice",
+    price: "$900",
+    img: "assets/fruite_juice.jpg",
+  },
+  {
+    id: 3,
+    title: "Yogurt Parfaite",
+    price: "$450",
+    img: "assets/yogurt_parfait.jpg",
+  },
+  {
+    id: 4,
+    title: "Orange Juice",
+    price: "$420",
+    img: "assets/orange_juice.webp",
+  },
+  {
+    id: 5,
+    title: "Egg & Plantain",
+    price: "$700",
+    img: "assets/edd_and_plantain.jpg",
+  },
+  {
+    id: 6,
+    title: "Tilapia Fish",
+    price: "$900",
+    img: "assets/tilapia_fish.jpg",
+  },
+  {
+    id: 7,
+    title: "Jollof Rice",
+    price: "$150",
+    img: "assets/jollof_rice.webp",
+  },
+  {
+    id: 8,
+    title: "Chiken Burger",
+    price: "$300",
+    img: "assets/chiken_burger.webp",
+  },
+  {
+    id: 9,
+    title: "Egg and Plantain",
+    price: "$500",
+    img: "assets/edd_and_plantain.jpg",
+  },
+  {
+    id: 10,
+    title: "African Salad",
+    price: "$200",
+    img: "assets/African_Salad.png",
+  },
+  {
+    id: 11,
+    title: "Chinese Pizza",
+    price: "$900",
+    img: "assets/chinese_pizza.jpg",
+  },
+];
+
+export const mealData = [
+  {
+    id: 5,
+    name: "cheeseless pizza",
+    category: "pizza",
+    image: "assets/cheeseless_pizza.jpg",
+    price: "$200",
+  },
+  {
+    id: 6,
+    name: "Supreme Pizza",
+    category: "pizza",
+    image: "assets/Supreme_Pizza.jpg",
+    price: "$300",
+  },
+  {
+    id: 7,
+    name: "pepperoni pizza",
+    category: "pizza",
+    image: "assets/pepperoni_pizza.jpg",
+    price: "$100",
+  },
+  {
+    id: 8,
+    name: "Cheese Pizza",
+    category: "pizza",
+    image: "assets/chinese_pizza.jpg",
+    price: "$140",
+  },
+  {
+    id: 8,
+    name: "African Salad",
+    category: "Salad",
+    image: "assets/African_Salad.png",
+    price: "$140",
+  },
+  {
+    id: 9,
+    name: "Kale Salad",
+    category: "salad",
+    image: "assets/kale_salad.jpg",
+    price: "$300",
+  },
+  {
+    id: 10,
+    name: "Ceasar Salad",
+    category: "salad",
+    image: "assets/caesar-salad-with-tunа.jpg",
+    price: "$300",
+  },
+  {
+    id: 11,
+    name: "Loaded Salad",
+    category: "salad",
+    image: "assets/loaded_salad.jpg",
+    price: "$200",
+  },
+  {
+    id: 12,
+    name: "chiken_shawarma",
+    category: "chiken",
+    image: "assets/chiken_shawarma.jpg",
+    price: "$100",
+  },
+  {
+    id: 13,
+    name: "Wings",
+    category: "chicken",
+    image: "assets/chicken-wings.jpg",
+    price: "$100",
+  },
+  {
+    id: 14,
+    name: "Roasted Chicken",
+    category: "chicken",
+    image: "assets/roast-chicken.jpg",
+    price: "$300",
+  },
+  {
+    id: 15,
+    name: "Chicken Tenders",
+    category: "chicken",
+    image: "assets/chicken_tenders.jpg",
+    price: "$100",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Burger",
+    image: "assets/burger_logo.png",
+  },
+  {
+    id: 2,
+    name: "veg",
+    image: "assets/veg_logo.png",
+  },
+  {
+    id: 3,
+    name: "Pizza",
+    image: "assets/pizza_logo.png",
+  },
+  {
+    id: 4,
+    name: "Wings",
+    image: "assets/wings_logo.png",
+  },
+
+  {
+    id: 5,
+    name: "Best Overall",
+    image: "assets/fastfood_logo.png",
+  },
+  {
+    id: 6,
+    name: "sharwama",
+    image: "assets/sharwama_logo.png",
+  },
+  {
+    id: 7,
+    name: "resottoa",
+    image: "assets/resotto_logo.png",
+  },
+  {
+    id: 8,
+    name: "cake",
+    image: "assets/cake.png",
+  },
+  {
+    id: 9,
+    name: "chiken",
+    image: "assets/chiken_logo.png",
+  },
+  {
+    id: 10,
+    name: "ice cream",
+    image: "assets/ice_cream_logo.png",
+  },
+  {
+    id: 11,
+    name: "beef",
+    image: "assets/beef_logo.png",
+  },
+  {
+    id: 12,
+    name: "apple pie",
+    image: "assets/appple_pie.png",
+  },
+];
